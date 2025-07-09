9b11921c1bd63a03637bf23dcbec004c24c1bf32
GIS <geokhaled29@gmail.com>
2025-07-09 23:03:48 +0300

    Update Geo_Khaled.py

--- Geo_Khaled.py
+++ Geo_Khaled.py
@@ -596,7 +596,7 @@ div[data-baseweb="slider"]::after{
   background:
       repeating-linear-gradient(
           to right,
-          transparent 4%, transparent 4%,
+          transparent 0%, transparent 4%,
           #888 4%, #888 5%,   /* خط بعرض 1% */
           transparent 5%, transparent 9%
       );
