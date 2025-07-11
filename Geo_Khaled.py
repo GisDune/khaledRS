7a92713c2020459faab6ad1814afc2df49978ac1
GIS <geokhaled29@gmail.com>
2025-07-11 17:30:57 +0300

    Update Geo_Khaled.py

--- Geo_Khaled.py
+++ Geo_Khaled.py
@@ -190,7 +190,7 @@ ul {
     height: 100vh !important;
     margin: 0 !important;
     padding: 0 !important;
-    background: url('https://raw.githubusercontent.com/GisDune/Khaled/main/e.webp') center/cover no-repeat !important;
+    background: url('https://i.ibb.co/PZc5c6Bn/e.webp') center/cover no-repeat !important;
     background-attachment: fixed;        /* تأثير Parallax خفيف على الحواسيب */
     display: flex !important;
     flex-direction: column !important;
